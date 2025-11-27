0fc1fa547fef0aad9e7c78ca0f30133dfc412ab0
samuelrincon422 <samuelangulo1124510@correo.itm.edu.co>
2025-11-27 17:41:31 -0500

    sistema de transaccion

--- Proseca.API/Controllers/VacunasController.cs
+++ Proseca.API/Controllers/VacunasController.cs
@@ -1,9 +1,9 @@
-﻿
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Proseca.API.Data;
+using Proseca.Shared.DTOs;
 using Proseca.Shared.Entidades;
 
 namespace Proseca.API.Controllers
@@ -83,6 +83,53 @@ namespace Proseca.API.Controllers
             else
             {
                 return NoContent();
+            }
+        }
+
+        [HttpPost("TransaccionVacuna")]
+        public async Task<ActionResult> CrearTransaccionVacuna(VacunaTransaccionDTO model)
+        {
+            using var transaction = await _context.Database.BeginTransactionAsync();
+
+            try
+            {
+                // Validamos que exista el animal y pertenezca a la finca
+                var animal = await _context.Animales.FindAsync(model.AnimalId);
+                if (animal == null || animal.FincaId != model.FincaId)
+                    return BadRequest("El animal no pertenece a la finca indicada.");
+
+                // 1) Registrar VACUNA
+                var vacuna = new Vacuna
+                {
+                    Nombre = model.NombreVacuna,
+                    Razon = model.Razon,
+                    AnimalId = model.AnimalId
+                };
+
+                _context.Vacunas.Add(vacuna);
+                await _context.SaveChangesAsync();
+
+                // 2) Registrar EVENTO DE SALUD
+                var evento = new EventoDeSalud
+                {
+                    Nombre = model.EventoNombre,
+                    Enfermedad = model.Enfermedad,
+                    FechaInicio = model.FechaInicio,
+                    FechaFin = model.FechaFin,
+                    Descripcion = model.Descripcion,
+                    AnimalId = model.AnimalId
+                };
+
+                _context.EventosDeSalud.Add(evento);
+                await _context.SaveChangesAsync();
+
+                await transaction.CommitAsync();
+                return Ok(new { mensaje = "Transacción exitosa", vacuna, evento });
+            }
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync();
+                return BadRequest("Error en la transacción → " + ex.Message);
             }
         }
     }
---
+++ Proseca.Shared/DTOs/VacunaTransaccionDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proseca.Shared.DTOs
+{
+    public class VacunaTransaccionDTO
+    {
+        public int AnimalId { get; set; }
+        public string NombreVacuna { get; set; }
+        public string Razon { get; set; }
+
+        // Relación con finca
+        public int FincaId { get; set; }
+
+        // Evento de Salud relacionado
+        public string EventoNombre { get; set; }
+        public string Enfermedad { get; set; }
+        public DateTime FechaInicio { get; set; } = DateTime.Now;
+        public DateTime FechaFin { get; set; } = DateTime.Now;
+        public string Descripcion { get; set; }
+    }
+}
